f54ffd98ccfc7a98bdde520e39bd03d1abf898af
Sait <said-bah@yandex.ru>
2024-11-12 20:40:28 +0300

    12 11 24

--- djangoProject_News_Portal/__init__.py
+++ djangoProject_News_Portal/__init__.py
@@ -1,1 +1,4 @@
 default_app_config = 'news_portal.apps.NewsPortalConfig'
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
---
+++ djangoProject_News_Portal/celery.py
@@ -0,0 +1,7 @@
+import os
+from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'djangoProject_News_Portal.settings')
+app = Celery('djangoProject_News_Portal')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
